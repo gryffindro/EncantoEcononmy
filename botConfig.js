28744d343b8655fe7ed53b9247435fe5dd6b8fe9
Gavyn Alejandro <gavyn.alejandro@outlook.com>
2022-01-30 16:14:53 -0500

    ready for deployment to heroku

--- botConfig.js
+++ botConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "OTM3MjM1NzkxMTE2MTQwNjA2.YfYy2Q.uWli-T8etZjBv3s6cyJlGuEO04g",
+    token: "OTM3MjM1NzkxMTE2MTQwNjA2.YfYy2Q.QRznzZYBf-JIw2zBxkQ1U-kmH5U",
     prefix: "$",
     admins: [
         "889640062369804299, 747213386869113015"
