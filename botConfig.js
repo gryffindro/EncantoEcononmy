1ab08bf8a17e8464e9479046dc671e12e485422b
Gavyn Alejandro <gavyn.alejandro@outlook.com>
2022-01-30 01:42:13 -0500

    Added Config. Made Commands
    Edited: Config.js

---
+++ botConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "OTM3MjM1NzkxMTE2MTQwNjA2.YfYy2Q.uWli-T8etZjBv3s6cyJlGuEO04g",
+    prefix: "$",
+    admins: [
+        "889640062369804299, 747213386869113015"
+],
+    debug: true,
+    countChannel: "933179874531749888"
+};
